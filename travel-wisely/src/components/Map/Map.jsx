3892ace9ba80b5f42c37c0c95a44ce3b10feb05d
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2022-10-16 19:01:28 -0400

    Add Map component and styles

--- travel-wisely/src/components/Map/Map.jsx
+++ travel-wisely/src/components/Map/Map.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+//protect API key later through environment variables
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import useStyles from "./styles";
 
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
 
-const Map = () => {
   return (
-    <h1>
-      Map
-    </h1>
-  )
-}
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
---
+++ travel-wisely/src/components/Map/styles.js
@@ -0,0 +1,24 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  paper: {
+    padding: "10px",
+    display: "flex",
+    width: "100px",
+    flexDirection: "column",
+    justifyContent: "center", 
+  },
+  mapContainer: {
+    height: "85vh",
+    width: "100%",
+  },
+  markerContainer: {
+    position: "absolute",
+    transform: "translate(-50%, -50%)",
+    zIndex: 1,
+    "&:hover": { zIndex: 2 },
+  },
+  pointer: {
+    cursor: "pointer",
+  }
+}));
